d2a92d59eaafe49e12f4506781ae0d02d16081b9
Temidayo Gabriel <dayotoru@gmail.com>
2026-06-17 13:04:01 -0400

    feat: initialize NestJS project with Drizzle ORM, Neon database, and Swagger documentation

---
+++ src/main.ts
@@ -0,0 +1,45 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+import CookieParser from 'cookie-parser';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+
+  app.use(CookieParser());
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('Nestjs Auth Api')
+    .setDescription('Complete authentication api starter ')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  const port = configService.get<number>('PORT') ?? 3000;
+
+  await app.listen(port);
+
+  console.log(`Application is running on : http://localhost:${port}/api`);
+  console.log(`Swagger docs is on : http://localhost:${port}/api/docs`);
+}
+bootstrap();
